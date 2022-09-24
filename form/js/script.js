7e5c723004d63dce21ff7d4ceb99d8e658a0ff8a
Fariaddd <89278138+Fariaddd@users.noreply.github.com>
2022-09-25 01:05:22 +0200

    sdd

--- form/js/script.js
+++ form/js/script.js
@@ -5,18 +5,12 @@ const firstName = document.querySelector("#firstName");
 const firstNameError = document.querySelector("#firstNameError");
 const lastName = document.querySelector("#lastName");
 const lastNameError = document.querySelector("#lastNameError");
+const emailError = document.querySelector("#emailError")
+const emailErrorNotValid = document.querySelector("#emailErrorNotValid")
 const email = document.querySelector("#email");
-// console.log(firstName);
-//console.log(contactForm);
-// console.log(firstNameError);
-// console.log(lastName);
-// console.log(email);
-contactForm.addEventListener("submit", function(event){
-    // console.log("here i clicked");
-    event.preventDefault();
 
-    // console.log("firstName", firstName.value);
-   
+contactForm.addEventListener("submit", function(event){
+    event.preventDefault();
     if(firstName.value.trim().length > 0){
         firstNameError.classList.add("hidden")
     } else {
@@ -26,6 +20,24 @@ contactForm.addEventListener("submit", function(event){
         lastNameError.classList.add("hidden")
     } else {
         lastNameError.classList.remove("hidden")
+    } 
+    if(email.value.trim().length && validateEmail(email.value) === true){
+        emailErrorNotValid.classList.add("hidden")
+    } 
+     else if (email.value.trim().length && validateEmail(email.value) !== true){
+        emailErrorNotValid.classList.remove("hidden")
+    }
+    if(email.value.trim().length > 0){
+        emailError.classList.add("hidden")
+    } else{
+        emailError.classList.remove("hidden")
     }
 })
-
+function validateEmail(email){
+    const regEx =   /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+    if(email.match(regEx)){
+        return true;
+    } else{
+        return false;
+    }     // or return !!email.match(regEx) instead of using if() statment
+}
